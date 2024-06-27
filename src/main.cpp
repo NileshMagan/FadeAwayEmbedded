ba0542cbf97c2b2f33aee621e4ca9fb794df3919
Nilesh Magan <nilesh.magan@rendernetworks.com>
2024-06-27 20:56:26 +1000

    Correct packets BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <ArduinoBLE.h>
 #include <Arduino_LSM6DS3.h>
 
-BLEService imuService("19b10010-e8f2-537e-4f6c-d104768a1214");                                        // Define a custom BLE service
-BLECharacteristic imuCharacteristic("19b10011-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify, 20); // Define a BLE characteristic for IMU data
+BLEService imuService("19b10010-e8f2-537e-4f6c-d104768a1214");
+BLECharacteristic imuCharacteristic("19b10011-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify, 50);
 
 void setup()
 {
@@ -25,14 +25,15 @@ void setup()
       ;
   }
 
-  BLE.setLocalName("Nano33IMU"); // Set a name for the Nano 33 BLE peripheral
+  BLE.setLocalName("Nano33IMU");
   BLE.setAdvertisedService(imuService);
-
   imuService.addCharacteristic(imuCharacteristic);
   BLE.addService(imuService);
-
-  imuCharacteristic.setValue("Hello IMU!"); // Initial dummy value
+  imuCharacteristic.setValue("Hello IMU!");
   BLE.advertise();
+
+  // Set min and max connection interval to 7.5 ms
+  BLE.setConnectionInterval(6, 6);
 
   Serial.println("BLE IMU Peripheral is now active, waiting for connections...");
 }
@@ -52,24 +53,18 @@ void loop()
 
       if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable())
       {
-        // Read accelerometer and gyroscope data
         IMU.readAcceleration(ax, ay, az);
         IMU.readGyroscope(gx, gy, gz);
 
-        // Format IMU data into a byte array
-        uint8_t imuData[20]; // Adjust the size as needed
-        snprintf((char *)imuData, sizeof(imuData), "%.2f,%.2f,%.2f,%.2f,%.2f,%.2f", ax, ay, az, gx, gy, gz);
+        // uint8_t imuData[50];
+        // snprintf((char *)imuData, sizeof(imuData), "%.2f,%.2f,%.2f,%.2f,%.2f,%.2f", ax, ay, az, gx, gy, gz);
+        // imuCharacteristic.setValue(imuData, sizeof(imuData));
+        char imuData[50];
+        snprintf(imuData, sizeof(imuData), "%.2f,%.2f,%.2f,%.2f,%.2f,%.2f", ax, ay, az, gx, gy, gz);
+        imuCharacteristic.setValue((uint8_t *)imuData, strlen(imuData));
 
-        imuCharacteristic.setValue(imuData, sizeof(imuData)); // Update BLE characteristic with IMU data
-        delay(100);                                           // Adjust delay as necessary for your application
+        delay(17); // Adjust delay to target 60 packets per second
       }
-      // float x, y, z;
-      // if (IMU.accelerationAvailable()) {
-      //   IMU.readAcceleration(x, y, z);
-      //   String data = String(x) + "," + String(y) + "," + String(z);
-      //   imuCharacteristic.writeValue(data.c_str());
-      //   delay(100);
-      // }
     }
 
     Serial.print("Disconnected from central: ");
